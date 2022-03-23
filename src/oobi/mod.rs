722a641d25458813a696919420bf794f6de65b1c
Edyta Pawlak <edyta.pawlakk@gmail.com>
2022-03-23 15:13:25 +0100

    feat(oobis): add database for oobis

--- src/oobi/mod.rs
+++ src/oobi/mod.rs
@@ -1,4 +1,4 @@
-use std::convert::TryFrom;
+use std::{convert::TryFrom, path::Path};
 
 use serde::{Deserialize, Serialize};
 use strum::{Display, EnumString};
@@ -108,10 +108,10 @@ pub struct OobiManager {
 }
 
 impl OobiManager {
-    pub fn new(validator: EventValidator) -> Self {
+    pub fn new(validator: EventValidator, oobi_db_path: &Path) -> Self {
         Self {
             validator,
-            store: OobiStorage::new(),
+            store: OobiStorage::new(oobi_db_path).unwrap(),
             queue: Responder::default(),
         }
     }
@@ -132,8 +132,9 @@ impl OobiManager {
             rpy.reply.check_digest()?;
 
             // check bada logic
-            if let Some(old_rpy) = self.store.get(&oobi_id)? {
-                bada_logic(rpy, &old_rpy)?;
+            if let Some(old_rpy) = self.store.get_full_reply(&oobi_id)? {
+                // TODO what if last isn't the most recent?
+                bada_logic(rpy, &old_rpy.last().unwrap())?;
             }
         };
 
@@ -155,13 +156,9 @@ impl OobiManager {
                 let msg = Message::try_from(sed).unwrap();
                 match msg {
                     Message::SignedOobi(oobi_rpy) => {
-                        match self.check_oobi_reply(&oobi_rpy) {
-                            Ok(_) => {
-                                self.store.save(oobi_rpy);
-                                Ok(())
-                            } // add to db,
-                            Err(_) => Err(Error::OobiError("Obi validation error".into())),
-                        }
+                        self.check_oobi_reply(&oobi_rpy)?;
+                        self.store.save(oobi_rpy)?;
+                        Ok(())
                     }
                     _ => Err(Error::OobiError("Wrong reply type".into())),
                 }
@@ -186,8 +183,11 @@ impl OobiManager {
     pub fn get_oobi(
         &self,
         id: &IdentifierPrefix,
-    ) -> Result<Option<EventMessage<ReplyEvent<Oobi>>>, Error> {
-        Ok(self.store.get(id)?.map(|e| e.reply))
+    ) -> Result<Option<Vec<EventMessage<ReplyEvent<Oobi>>>>, Error> {
+        Ok(self
+            .store
+            .get_full_reply(id)?
+            .map(|e_list| e_list.into_iter().map(|e| e.reply).collect()))
     }
 }
 mod error {
@@ -201,6 +201,8 @@ mod error {
         UrlParsingError(#[from] url::ParseError),
         #[error("Keri error occured")]
         KeriError(#[from] crate::error::Error),
+        #[error(transparent)]
+        SledError(#[from] sled::Error),
     }
 }
 
@@ -232,29 +234,50 @@ async fn test_obi_save() -> Result<(), Error> {
     let body = r#"{"v":"KERI10JSON0000fa_","t":"rpy","d":"EJq4dQQdqg8aK7VyGnfSibxPyW8Zk2zO1qbVRD6flOvE","dt":"2022-02-28T17:23:20.336207+00:00","r":"/loc/scheme","a":{"eid":"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","scheme":"http","url":"http://127.0.0.1:5643/"}}-VAi-CABBuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw0BAPJ5p_IpUFdmq8uupehsL8DzxWDeaU_SjeiwfmRZ6i9pqddraItmCOAysdXdTEQZ1hEM60iDEWvK16g68TrcAw{"v":"KERI10JSON0000f8_","t":"rpy","d":"ExSR01j5noF2LnGcGFUbLnq-U8JuYBr9WWEMt8d2fb1Y","dt":"2022-02-28T17:23:20.337272+00:00","r":"/loc/scheme","a":{"eid":"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","scheme":"tcp","url":"tcp://127.0.0.1:5633/"}}-VAi-CABBuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw0BZtIhK6Nh6Zk1zPmkJYiFVz0RimQRiubshmSmqAzxzhT4KpGMAH7sbNlFP-0-lKjTawTReKv4L7N3TR7jxXaEBg{"v":"KERI10JSON000116_","t":"rpy","d":"EcZ1I4nKy6gIkWxjq1LmIivoPGv32lvlSuMVsWnOPwSc","dt":"2022-02-28T17:23:20.338355+00:00","r":"/end/role/add","a":{"cid":"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","role":"controller","eid":"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw"}}-VAi-CABBuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw0B9ccIiMxdwurRjGvUUUdXsxhseo58onhE4bJddKuyPaSpBHXdRKKuiFE0SmLAogMQGJ0iN6f1V_2E_MVfMc3sAA"#;
     let stream = signed_event_stream(body.as_bytes());
     assert_eq!(stream.unwrap().1.len(), 2);
-
     use tempfile::Builder;
 
     // Create test db and event processor.
     let root = Builder::new().prefix("test-db").tempdir().unwrap();
     fs::create_dir_all(root.path()).unwrap();
+    let oobi_root = Builder::new().prefix("oobi-test-db").tempdir().unwrap();
+    fs::create_dir_all(oobi_root.path()).unwrap();
 
     let db = Arc::new(SledEventDatabase::new(root.path()).unwrap());
     let validator = EventValidator::new(Arc::clone(&db));
 
-    let oobi_manager = OobiManager::new(validator);
+    let oobi_manager = OobiManager::new(validator, oobi_root.path());
+
+    let wrong_body = r#"{"v":"KERI10JSON0000fa_","t":"rpy","d":"EJq4dQQdqg8aK7VyGnfSibxPyW8Zk2zO1qbVRD6flOvE","dt":"2022-02-28T17:23:20.336207+00:00","r":"/loc/scheme","a":{"eid":"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","scheme":"http","url":"http://127.0.0.1:5643/"}}-VAi-CABBuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw0BAPJ5p_IpUFdmq8uupehsL8DzxWDeaU_SjeiwfmRZ6i9pqddraItmCOAysdXdTEQZ1hEM60iDEWvK16g68TrcAA"#;
+    assert!(matches!(
+        oobi_manager.parse_and_save(wrong_body),
+        Err(Error::KeriError(
+            crate::error::Error::SignatureVerificationError
+        ))
+    ));
+    let res = oobi_manager.store.get_urls(
+        &"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw"
+            .parse::<IdentifierPrefix>()
+            .unwrap(),
+    )?;
+    assert_eq!(res, None);
 
     oobi_manager.parse_and_save(body)?;
     // oobi_manager.process_oobi(
     //     "http://127.0.0.1:5643/oobi/BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw/controller",
     // )?;
     // oobi_manager.load().await?;
-    let res = oobi_manager.store.get(
+    let res = oobi_manager.store.get_urls(
         &"BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw"
             .parse::<IdentifierPrefix>()
             .unwrap(),
     )?;
-    assert!(res.is_some());
+    assert_eq!(
+        res,
+        Some(vec![
+            Url::parse("http://127.0.0.1:5643/").unwrap(),
+            Url::parse("tcp://127.0.0.1:5633/").unwrap()
+        ])
+    );
 
     Ok(())
 }
