4289ccb3ce8e517b477c032b6b84f981540a8053
Nikita Stefaniak <lisqxd@pm.me>
2022-04-20 10:43:00 +0200

    gossip improvements

--- src/gossip/mod.rs
+++ src/gossip/mod.rs
@@ -9,19 +9,24 @@ use std::{
     collections::{hash_map::Entry, HashMap},
     io,
     net::SocketAddr,
+    ops::{Deref, DerefMut},
     sync::Arc,
     time::Duration,
 };
 
-use async_std::{net::UdpSocket, sync::Mutex, task::sleep};
-use rand::prelude::IteratorRandom;
+use async_std::{
+    net::UdpSocket,
+    sync::{Mutex, MutexGuard, RwLock, RwLockReadGuard},
+    task::sleep,
+};
+use rand::{prelude::IteratorRandom, Rng};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 /// Gossip server instance.
 pub struct Server<T: Clone> {
     socket: UdpSocket,
-    network: Arc<Mutex<Network<T>>>,
+    network: Network<T>,
 }
 
 impl<T> Server<T>
@@ -30,9 +35,9 @@ where
 {
     /// Initializes a new Gossip server instance with initial local data.
     /// Opens a UDP socket immediately.
-    pub async fn new(data: T) -> io::Result<Self> {
-        let socket = UdpSocket::bind("0.0.0.0:0").await?;
-        let network = Arc::new(Mutex::new(Network::new(data)));
+    pub async fn new(data: T, port: u16) -> io::Result<Self> {
+        let socket = UdpSocket::bind(SocketAddr::from(([0, 0, 0, 0], port))).await?;
+        let network = Network::new(data);
         Ok(Self { socket, network })
     }
 
@@ -51,29 +56,20 @@ where
         self.send(peer_addr).await
     }
 
-    /// Modifies the data associated with local peer.
-    pub async fn update_data(&self, update_fn: impl FnOnce(&mut T)) {
-        let mut network = self.network.lock().await;
-        network.local_version += 1;
-        update_fn(&mut network.local_data);
-    }
-
-    /// Returns data about all the peers including local peer.
-    pub async fn get_peers(&self) -> PeerMap<T> {
-        let network = self.network.lock().await;
-        network.peers.clone()
+    /// Returns data about all the known peers.
+    /// Can include data about local peer too.
+    pub async fn peers(&self) -> RwLockReadGuard<'_, PeerMap<T>> {
+        self.network.peers.read().await
     }
 
     /// Returns the data associated with local peer.
-    pub async fn get_data(&self) -> T {
-        let network = self.network.lock().await;
-        network.local_data.clone()
+    pub async fn data(&self) -> MutexGuard<'_, Data<T>> {
+        self.network.data.lock().await
     }
 
     /// Returns ID of the local peer.
-    pub async fn get_id(&self) -> Uuid {
-        let network = self.network.lock().await;
-        network.local_id
+    pub async fn id(&self) -> Uuid {
+        self.network.id
     }
 
     /// Starts the receiving loop.
@@ -82,54 +78,74 @@ where
 
         loop {
             let (len, peer_addr) = self.socket.recv_from(&mut buf[..]).await?;
-            let msg = &buf[..len];
-            let msg: Message<T> = bincode::deserialize(msg)?;
+            log::info!("{} receiving from {}", self.network.id, peer_addr);
+            let buf = &buf[..len];
+            let msg = bincode::deserialize::<Message<T>>(buf)?;
             self.recv(msg, peer_addr).await?;
         }
     }
 
     /// Handles a single incoming message.
-    async fn recv(&self, msg: Message<T>, peer_addr: SocketAddr) -> Result<(), Error> {
-        let mut network = self.network.lock().await;
-
+    async fn recv(&self, msg: Message<T>, remote_addr: SocketAddr) -> Result<(), Error> {
         match msg {
-            Message::Sync { state } => {
-                let mut new_peers = state.peers;
-
-                // check if we have some data that the other peer doesn't
-                let has_more = network
-                    .peers
-                    .iter()
-                    .any(|peer| !new_peers.contains_key(peer.0));
-
+            Message::Sync {
+                id: sender_id,
+                data: sender_data,
+                peers: mut sender_peers,
+            } => {
                 // add sender of the message to the new peer list too
-                new_peers.insert(
-                    state.local_id,
+                sender_peers.insert(
+                    sender_id,
                     Peer {
-                        id: state.local_id,
-                        addr: peer_addr,
-                        version: state.local_version,
-                        data: state.local_data,
+                        addr: remote_addr,
+                        data: sender_data,
                     },
                 );
 
+                log::info!(
+                    "{} received {} peers from {}",
+                    self.network.id,
+                    sender_peers.len(),
+                    remote_addr
+                );
+
+                let mut local_peers = self.network.peers.write().await;
+
                 // update our peer list with new data
-                for (new_peer_id, new_peer) in new_peers {
-                    match network.peers.entry(new_peer_id) {
+                for (new_peer_id, new_peer) in &sender_peers {
+                    match local_peers.entry(*new_peer_id) {
                         Entry::Vacant(entry) => {
-                            entry.insert(new_peer);
+                            entry.insert(new_peer.clone());
                         }
                         Entry::Occupied(mut entry) => {
-                            if new_peer.version > entry.get().version {
-                                *entry.get_mut() = new_peer;
+                            if new_peer.data.version > entry.get().data.version {
+                                *entry.get_mut() = new_peer.clone();
                             }
                         }
                     }
                 }
 
+                // check if the sender needs updating
+                let mut remote_needs_update = false;
+                for (peer_id, peer) in &*local_peers {
+                    let remote_peer = sender_peers.get(&peer_id);
+                    if let Some(remote_peer) = remote_peer {
+                        if remote_peer.data.version >= peer.data.version {
+                            continue;
+                        }
+                    }
+                    remote_needs_update = true;
+                    break;
+                }
+
+                drop(local_peers);
+
                 // if we have more data then update the other peer too
-                if has_more {
-                    self.send(peer_addr).await?;
+                if remote_needs_update {
+                    log::info!("{} reply to {}", self.network.id, remote_addr);
+                    if let Err(err) = self.send(remote_addr).await {
+                        log::error!("{} reply failed: {}", self.network.id, err);
+                    };
                 }
             }
         }
@@ -139,25 +155,29 @@ where
 
     /// Starts the sending loop
     async fn send_loop(&self) -> Result<(), Error> {
+        let mut rng = rand::thread_rng();
         loop {
             // delay between sends
-            sleep(Duration::from_secs(5)).await;
+            sleep(Duration::from_millis(rng.gen_range(3000, 4000))).await;
 
             // select 3 random peers
-            let recipients = {
-                let mut rng = rand::thread_rng();
-                let network = self.network.lock().await;
-                network
-                    .peers
-                    .iter()
-                    .map(|(_, peer)| peer.addr)
-                    .choose_multiple(&mut rng, 3)
-            };
+            let recipients = self
+                .network
+                .peers
+                .read()
+                .await
+                .iter()
+                .map(|(_, peer)| peer.addr)
+                .choose_multiple(&mut rng, 3);
+
+            log::info!("{} sending to {} peers", self.network.id, recipients.len());
 
             // send
             // TODO: use try_join_all
             for addr in recipients {
-                self.send(addr).await?;
+                if let Err(err) = self.send(addr).await {
+                    log::error!("{} sending failed: {}", self.network.id, err);
+                };
             }
         }
     }
@@ -165,8 +185,17 @@ where
     /// Sends a single message to a single peer.
     async fn send(&self, recipient_addr: SocketAddr) -> Result<(), Error> {
         // prepare message
+        let peers = self.network.peers.read().await.clone();
+        log::info!(
+            "{} sending {} peers to {}",
+            self.network.id,
+            peers.len(),
+            recipient_addr
+        );
         let msg = Message::Sync {
-            state: (self.network.lock().await).clone(),
+            id: self.network.id,
+            data: self.network.data.lock().await.clone(),
+            peers,
         };
         let msg = bincode::serialize(&msg)?;
 
@@ -178,12 +207,10 @@ where
 }
 
 /// The network state as known to a single peer
-#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Network<T: Clone> {
-    local_id: Uuid,
-    local_version: u32,
-    local_data: T,
-    peers: PeerMap<T>,
+    id: Uuid,
+    data: Arc<Mutex<Data<T>>>,
+    peers: Arc<RwLock<PeerMap<T>>>,
 }
 
 impl<T> Network<T>
@@ -192,10 +219,12 @@ where
 {
     pub fn new(data: T) -> Self {
         Self {
-            local_id: Uuid::new_v4(),
-            local_data: data,
-            local_version: 0,
-            peers: HashMap::new(),
+            id: Uuid::new_v4(),
+            data: Arc::new(Mutex::new(Data {
+                value: data,
+                version: 0,
+            })),
+            peers: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 }
@@ -203,7 +232,17 @@ where
 /// Message that can be sent between peers.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 enum Message<T: Clone> {
-    Sync { state: Network<T> },
+    /// Sent to another peer to update its state.
+    /// If the receiver detects that it has some newer data it should reply with another Sync.
+    Sync {
+        /// Sender ID
+        id: Uuid,
+        /// Sender data
+        data: Data<T>,
+        /// Other peers known to sender.
+        /// Can include the sender data too.
+        peers: PeerMap<T>,
+    },
 }
 
 type PeerMap<T> = HashMap<Uuid, Peer<T>>;
@@ -211,10 +250,39 @@ type PeerMap<T> = HashMap<Uuid, Peer<T>>;
 /// Snapshot of other peer's state.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Peer<T: Clone> {
-    id: Uuid,
     addr: SocketAddr,
+    data: Data<T>,
+}
+
+/// Snapshot of other peer's data.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Data<T: Clone> {
     version: u32,
-    data: T,
+    value: T,
+}
+
+impl<T> Data<T>
+where
+    T: Clone + Send + Sync + Serialize + for<'de> Deserialize<'de>,
+{
+    pub fn new(value: T) -> Self {
+        Self { value, version: 0 }
+    }
+}
+
+impl<T: Clone> Deref for Data<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T: Clone> DerefMut for Data<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.version += 1;
+        &mut self.value
+    }
 }
 
 /// Gossip server error.
