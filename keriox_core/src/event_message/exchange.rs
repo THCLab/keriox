4437a59a90ac2cdac68d786ace6fc05a6a837ce3
Edyta Pawlak <edyta.pawlakk@gmail.com>
2022-10-25 14:15:38 +0200

    Fix exn test

--- keriox_core/src/event_message/exchange.rs
+++ keriox_core/src/event_message/exchange.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     derivation::self_addressing::SelfAddressing, error::Error, event::SerializationFormats,
-    event_parsing::path::MaterialPath, prefix::IdentifierPrefix,
+    event_parsing::path::MaterialPath, prefix::IdentifierPrefix, query::Timestamped,
 };
 
 use super::{
@@ -10,7 +10,7 @@ use super::{
     Typeable,
 };
 
-pub type ExchangeMessage = EventMessage<SaidEvent<Exchange>>;
+pub type ExchangeMessage = EventMessage<SaidEvent<Timestamped<Exchange>>>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct SignedExchange {
@@ -37,8 +37,8 @@ impl Exchange {
         self,
         format: SerializationFormats,
         derivation: &SelfAddressing,
-    ) -> Result<EventMessage<SaidEvent<Exchange>>, Error> {
-        SaidEvent::<Exchange>::to_message(self, format, derivation)
+    ) -> Result<ExchangeMessage, Error> {
+        SaidEvent::<Timestamped<Exchange>>::to_message(Timestamped::new(self), format, derivation)
     }
 }
 
@@ -75,17 +75,17 @@ impl Typeable for Exchange {
 
 #[test]
 fn test_exn_serialization() -> Result<(), Error> {
-    let exn_event = r#"{"v":"KERI10JSON0002c9_","t":"exn","d":"Eru6l4p3-r6KJkT1Ac8r5XWuQMsD91-c80hC7lASOoZI","r":"/fwd","q":{"pre":"E-4-PsMBN0YEKyTl3zL0zulWcBehdaaG6Go5cMc0BzQ8","topic":"multisig"},"a":{"v":"KERI10JSON000215_","t":"icp","d":"EOWwyMU3XA7RtWdelFt-6waurOTH_aW_Z9VTaU-CshGk","i":"EOWwyMU3XA7RtWdelFt-6waurOTH_aW_Z9VTaU-CshGk","s":"0","kt":"2","k":["DQKeRX-2dXdSWS-EiwYyiQdeIwesvubEqnUYC5vsEyjo","D-U6Sc6VqQC3rDuD2wLF3oR8C4xQyWOTMp4zbJyEnRlE"],"nt":"2","n":["ENVtv0_G68psQhfWB-ZyVH1lndLli2LSmfSxxszNufoI","E6UpCouA9mZA03hMFJLrhA0SvwR4HVNqf2wrZM-ydTSI"],"bt":"3","b":["BGKVzj4ve0VSd8z_AmvhLg4lqcC_9WYX90k03q-R_Ydo","BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","Bgoq68HCmYNUDgOz4Skvlu306o_NY-NrYuKAVhk3Zh9c"],"c":[],"a":[]}}"#;
+    let exn_event = r#"{"v":"KERI10JSON0002f1_","t":"exn","d":"EPfS_lQ-hZIFX6ug1ggLlzVN09VnCWsubpE-jAC1Fx0W","dt":"2022-10-25T09:53:04.117732+00:00","r":"/fwd","q":{"pre":"EJccSRTfXYF6wrUVuenAIHzwcx3hJugeiJsEKmndi5q1","topic":"multisig"},"a":{"v":"KERI10JSON000215_","t":"icp","d":"EC61gZ9lCKmHAS7U5ehUfEbGId5rcY0D7MirFZHDQcE2","i":"EC61gZ9lCKmHAS7U5ehUfEbGId5rcY0D7MirFZHDQcE2","s":"0","kt":"2","k":["DOZlWGPfDHLMf62zSFzE8thHmnQUOgA3_Y-KpOyF9ScG","DHGb2qY9WwZ1sBnC9Ip0F-M8QjTM27ftI-3jTGF9mc6K"],"nt":"2","n":["EBvD5VIVvf6NpP9GRmTqu_Cd1KN0RKrKNfPJ-uhIxurj","EHlpcaxffvtcpoUUMTc6tpqAVtb2qnOYVk_3HRsZ34PH"],"bt":"3","b":["BBilc4-L3tFUnfM_wJr4S4OJanAv_VmF_dJNN6vkf2Ha","BLskRTInXnMxWaGqcpSyMgo0nYbalW99cGZESrz3zapM","BIKKuvBwpmDVA4Ds-EpL5bt9OqPzWPja2LigFYZN2YfX"],"c":[],"a":[]}}"#; //-HABEKYLUMmNPZeEs77Zvclf0bSN5IN-mLfLpx2ySb-HDlk4-AABAACJvddJrANYlrJ7CxEU9Z_AKJMxJZ7PNSyZeS4F6x2qZ2vTLtmD6mOOQ748TlddgB2ZFAMYt3xtzNdfrYNHS4IA-LAZ5AABAA-a-AABABBng3jTIIx_YUX-tS0caV1aV9QOvD5IM7WKt_wQz6Hvjm7nPhJgElP6K4Pu2JAIqCO93wBgBOx1DD3iawt0rb4"#;
 
     let parsed: ExchangeMessage = serde_json::from_str(exn_event).unwrap();
-    let ser_deser = serde_json::to_string(&parsed).unwrap();
+    let ser_deser = String::from_utf8(parsed.serialize()?).unwrap(); // serde_json::to_string(&parsed).unwrap();
 
     assert_eq!(exn_event, ser_deser);
 
-    let exchange = r#"{"v":"KERI10JSON0002fd_","t":"exn","d":"E4eI8YwFFm3FgrFetNtk9n5uunKaacICRdmuIe9cX_VE","r":"/fwd","q":{"pre":"E8AKUcbZyik8EdkOwXgnyAxO5mSIPJWGZ_o7zMhnNnjo","topic":"delegate"},"a":{"v":"KERI10JSON000249_","t":"dip","d":"EcUCJqbf43LPLDlo2ulpsPQCxhuYwaiiHoTuU9cET89s","i":"EcUCJqbf43LPLDlo2ulpsPQCxhuYwaiiHoTuU9cET89s","s":"0","kt":"2","k":["DQKeRX-2dXdSWS-EiwYyiQdeIwesvubEqnUYC5vsEyjo","D-U6Sc6VqQC3rDuD2wLF3oR8C4xQyWOTMp4zbJyEnRlE"],"nt":"2","n":["ENVtv0_G68psQhfWB-ZyVH1lndLli2LSmfSxxszNufoI","E6UpCouA9mZA03hMFJLrhA0SvwR4HVNqf2wrZM-ydTSI"],"bt":"3","b":["BGKVzj4ve0VSd8z_AmvhLg4lqcC_9WYX90k03q-R_Ydo","BuyRFMideczFZoapylLIyCjSdhtqVb31wZkRKvPfNqkw","Bgoq68HCmYNUDgOz4Skvlu306o_NY-NrYuKAVhk3Zh9c"],"c":[],"a":[],"di":"E8AKUcbZyik8EdkOwXgnyAxO5mSIPJWGZ_o7zMhnNnjo"}}"#;
+    let exchange = r#"{"v":"KERI10JSON000325_","t":"exn","d":"EJLdWmOy2wj3GfoPP5A1eIYAuP5fqpOUtWYgEjPG7DZp","dt":"2022-10-25T12:04:30.636995+00:00","r":"/fwd","q":{"pre":"EHpD0-CDWOdu5RJ8jHBSUkOqBZ3cXeDVHWNb_Ul89VI7","topic":"delegate"},"a":{"v":"KERI10JSON000249_","t":"dip","d":"EL_Atfv-taLFJVpu1Gzy50hCjsJ5Qvn7_sH1kuCGgwvl","i":"EL_Atfv-taLFJVpu1Gzy50hCjsJ5Qvn7_sH1kuCGgwvl","s":"0","kt":"2","k":["DOZlWGPfDHLMf62zSFzE8thHmnQUOgA3_Y-KpOyF9ScG","DHGb2qY9WwZ1sBnC9Ip0F-M8QjTM27ftI-3jTGF9mc6K"],"nt":"2","n":["EBvD5VIVvf6NpP9GRmTqu_Cd1KN0RKrKNfPJ-uhIxurj","EHlpcaxffvtcpoUUMTc6tpqAVtb2qnOYVk_3HRsZ34PH"],"bt":"3","b":["BBilc4-L3tFUnfM_wJr4S4OJanAv_VmF_dJNN6vkf2Ha","BLskRTInXnMxWaGqcpSyMgo0nYbalW99cGZESrz3zapM","BIKKuvBwpmDVA4Ds-EpL5bt9OqPzWPja2LigFYZN2YfX"],"c":[],"a":[],"di":"EHpD0-CDWOdu5RJ8jHBSUkOqBZ3cXeDVHWNb_Ul89VI7"}}"#; //-HABEJccSRTfXYF6wrUVuenAIHzwcx3hJugeiJsEKmndi5q1-AABAAAoAXJY8aWoWNgT6p9ww2XWOTJtOvcr1Y3Ej-XigsYyEufoj-vixPK8_en9PH3DFZwDf3vnwr4MbYOUU2h09kYD-LAv5AABAA-a-AACAABgMl2dGcyy8zT0x7HypyAEePk3R5WBjuPeA1hJRq-a4-8_F_8Dr4CLJGnyxkomJUSTODhv46UDCjdZZPXKMScMABDaYeZ1E7ekpj9qFFuwQnmrKq4H4LrlgTiGjeJ1aVLSDkM5f7UoUOCcPlwuNubhnS_69zT5SdDZMTXWKLtbJnUF"#;
 
     let parsed: ExchangeMessage = serde_json::from_str(exchange).unwrap();
-    let ser_deser = serde_json::to_string(&parsed).unwrap();
+    let ser_deser = String::from_utf8(parsed.serialize()?).unwrap();
 
     assert_eq!(exchange, ser_deser);
     Ok(())
