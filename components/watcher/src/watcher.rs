db887013ea898229f567846c3c53d5717af86139
Edyta Pawlak <edyta.pawlakk@gmail.com>
2022-12-13 13:32:07 +0100

    Set escrows in watcher

--- components/watcher/src/watcher.rs
+++ components/watcher/src/watcher.rs
@@ -10,12 +10,12 @@ use keri::{
         parse_notice_stream, parse_query_stream, parse_reply_stream, prelude::*,
         simple_controller::PossibleResponse,
     },
-    database::DbError,
+    database::{DbError, escrow::EscrowDb},
     error::Error,
     event_message::signed_event_message::{Notice, Op},
     oobi::{error::OobiError, EndRole, LocationScheme, OobiManager, Role, Scheme},
     prefix::{AttachedSignaturePrefix, BasicPrefix, IdentifierPrefix, SelfSigningPrefix},
-    processor::{escrow::ReplyEscrow, notification::JustNotification},
+    processor::{escrow::default_escrow_bus},
     query::{
         query_event::{QueryArgs, QueryEvent, QueryRoute, SignedQuery},
         reply_event::{ReplyEvent, ReplyRoute, SignedReply},
@@ -54,16 +54,17 @@ impl WatcherData {
         oobi_path.push(event_db_path);
         oobi_path.push("oobi");
 
-        let prefix = BasicPrefix::Ed25519NT(signer.public_key()); // watcher uses non transferable key
+        let mut escrow_path = PathBuf::new();
+        escrow_path.push(event_db_path);
+        escrow_path.push("escrow");
+
         let db = Arc::new(SledEventDatabase::new(event_db_path)?);
-        let mut processor = BasicProcessor::new(db.clone(), None);
-        processor.register_observer(
-            Arc::new(ReplyEscrow::new(db.clone())),
-            &[
-                JustNotification::KeyEventAdded,
-                JustNotification::KsnOutOfOrder,
-            ],
-        )?;
+        let escrow_db = Arc::new(EscrowDb::new(escrow_path.as_path())?);
+        let (notification_bus, _) =
+            default_escrow_bus(db.clone(), escrow_db);
+
+        let prefix = BasicPrefix::Ed25519NT(signer.public_key()); // watcher uses non transferable key
+        let processor = BasicProcessor::new(db.clone(), Some(notification_bus));
         let storage = EventStorage::new(db.clone());
         // construct witness loc scheme oobi
         let loc_scheme = LocationScheme::new(
