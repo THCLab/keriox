ae9e155a2f8ff5d310fe7f98d3638f9f48c77607
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-05-09 13:58:37 +0200

    Add mailbox submodule

---
+++ keriox_core/src/database/mailbox.rs
@@ -0,0 +1,83 @@
+use sled::Db;
+
+use crate::event_message::signed_event_message::{SignedNontransferableReceipt, SignedEventMessage};
+
+use super::{tables::SledEventTreeVec, timestamped::TimestampedSignedEventMessage, DbError};
+
+pub struct MailboxData {
+    mailbox_receipts: SledEventTreeVec<SignedNontransferableReceipt>,
+    mailbox_replies: SledEventTreeVec<SignedEventMessage>,
+    mailbox_multisig: SledEventTreeVec<TimestampedSignedEventMessage>,
+    mailbox_delegate: SledEventTreeVec<TimestampedSignedEventMessage>,
+}
+
+impl MailboxData {
+    pub fn new(db: &Db) -> Result<Self, DbError> {
+        Ok(Self {
+            mailbox_receipts: SledEventTreeVec::new(db.open_tree(b"mbxrct")?),
+            mailbox_replies: SledEventTreeVec::new(db.open_tree(b"mbxrpy")?),
+            mailbox_multisig: SledEventTreeVec::new(db.open_tree(b"mbxm")?),
+            mailbox_delegate: SledEventTreeVec::new(db.open_tree(b"mbxd")?),
+        })
+    }
+
+    pub fn add_mailbox_receipt(
+        &self,
+        key: u64,
+        receipt: SignedNontransferableReceipt,
+    ) -> Result<(), DbError> {
+        if !self.mailbox_receipts.contains_value(&receipt) {
+            self.mailbox_receipts.push(key, receipt)
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn get_mailbox_receipts(
+        &self,
+        key: u64,
+    ) -> Option<impl DoubleEndedIterator<Item = SignedNontransferableReceipt>> {
+        self.mailbox_receipts.iter_values(key)
+    }
+
+    pub fn add_mailbox_reply(&self, key: u64, reply: SignedEventMessage) -> Result<(), DbError> {
+        if !self.mailbox_replies.contains_value(&reply) {
+            self.mailbox_replies.push(key, reply)
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn get_mailbox_replies(
+        &self,
+        key: u64,
+    ) -> Option<impl DoubleEndedIterator<Item = SignedEventMessage>> {
+        self.mailbox_replies.iter_values(key)
+    }
+
+    pub fn add_mailbox_multisig(&self, key: u64, event: SignedEventMessage) -> Result<(), DbError> {
+        self.mailbox_multisig.push(key, event.into())
+    }
+
+    pub fn get_mailbox_multisig(
+        &self,
+        key: u64,
+    ) -> Option<impl DoubleEndedIterator<Item = TimestampedSignedEventMessage>> {
+        self.mailbox_multisig.iter_values(key)
+    }
+
+    pub fn add_mailbox_delegate(
+        &self,
+        key: u64,
+        delegated: SignedEventMessage,
+    ) -> Result<(), DbError> {
+        self.mailbox_delegate.push(key, delegated.into())
+    }
+
+    pub fn get_mailbox_delegate(
+        &self,
+        key: u64,
+    ) -> Option<impl DoubleEndedIterator<Item = TimestampedSignedEventMessage>> {
+        self.mailbox_delegate.iter_values(key)
+    }
+}
--- keriox_core/src/database/mod.rs
+++ keriox_core/src/database/mod.rs
@@ -1,14 +1,14 @@
 pub mod escrow;
 pub(crate) mod tables;
 pub(crate) mod timestamped;
+#[cfg(feature = "mailbox")]
+pub mod mailbox;
 
 use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
-#[cfg(feature = "mailbox")]
-use sled::Db;
 
-use self::tables::{SledEventTree, SledEventTreeVec};
+use self::{tables::{SledEventTree, SledEventTreeVec}, mailbox::MailboxData};
 #[cfg(feature = "query")]
 use crate::query::reply_event::SignedReply;
 use crate::{
@@ -25,85 +25,6 @@ use crate::{
 
 use self::timestamped::TimestampedSignedEventMessage;
 
-#[cfg(feature = "mailbox")]
-pub struct MailboxData {
-    mailbox_receipts: SledEventTreeVec<SignedNontransferableReceipt>,
-    mailbox_replies: SledEventTreeVec<SignedEventMessage>,
-    mailbox_multisig: SledEventTreeVec<TimestampedSignedEventMessage>,
-    mailbox_delegate: SledEventTreeVec<TimestampedSignedEventMessage>,
-}
-
-#[cfg(feature = "mailbox")]
-impl MailboxData {
-    pub fn new(db: &Db) -> Result<Self, DbError> {
-        Ok(Self {
-            mailbox_receipts: SledEventTreeVec::new(db.open_tree(b"mbxrct")?),
-            mailbox_replies: SledEventTreeVec::new(db.open_tree(b"mbxrpy")?),
-            mailbox_multisig: SledEventTreeVec::new(db.open_tree(b"mbxm")?),
-            mailbox_delegate: SledEventTreeVec::new(db.open_tree(b"mbxd")?),
-        })
-    }
-
-    pub fn add_mailbox_receipt(
-        &self,
-        key: u64,
-        receipt: SignedNontransferableReceipt,
-    ) -> Result<(), DbError> {
-        if !self.mailbox_receipts.contains_value(&receipt) {
-            self.mailbox_receipts.push(key, receipt)
-        } else {
-            Ok(())
-        }
-    }
-
-    pub fn get_mailbox_receipts(
-        &self,
-        key: u64,
-    ) -> Option<impl DoubleEndedIterator<Item = SignedNontransferableReceipt>> {
-        self.mailbox_receipts.iter_values(key)
-    }
-
-    pub fn add_mailbox_reply(&self, key: u64, reply: SignedEventMessage) -> Result<(), DbError> {
-        if !self.mailbox_replies.contains_value(&reply) {
-            self.mailbox_replies.push(key, reply)
-        } else {
-            Ok(())
-        }
-    }
-
-    pub fn get_mailbox_replies(
-        &self,
-        key: u64,
-    ) -> Option<impl DoubleEndedIterator<Item = SignedEventMessage>> {
-        self.mailbox_replies.iter_values(key)
-    }
-
-    pub fn add_mailbox_multisig(&self, key: u64, event: SignedEventMessage) -> Result<(), DbError> {
-        self.mailbox_multisig.push(key, event.into())
-    }
-
-    pub fn get_mailbox_multisig(
-        &self,
-        key: u64,
-    ) -> Option<impl DoubleEndedIterator<Item = TimestampedSignedEventMessage>> {
-        self.mailbox_multisig.iter_values(key)
-    }
-
-    pub fn add_mailbox_delegate(
-        &self,
-        key: u64,
-        delegated: SignedEventMessage,
-    ) -> Result<(), DbError> {
-        self.mailbox_delegate.push(key, delegated.into())
-    }
-
-    pub fn get_mailbox_delegate(
-        &self,
-        key: u64,
-    ) -> Option<impl DoubleEndedIterator<Item = TimestampedSignedEventMessage>> {
-        self.mailbox_delegate.iter_values(key)
-    }
-}
 
 pub struct SledEventDatabase {
     // "iids" tree
