ddd02895eeea4599455bc0bcefd06a07a3d8edba
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-03-20 16:33:43 +0100

    tests: add comments

--- keriox_tests/tests/test_kel_with_witness.rs
+++ keriox_tests/tests/test_kel_with_witness.rs
@@ -31,11 +31,17 @@ async fn test_kel_with_witness() -> Result<(), ControllerError> {
     let database_path = Builder::new().prefix("test-db0").tempdir().unwrap();
     let mut key_manager = CryptoBox::new().unwrap();
 
+    // The `Controller` structure aggregates all known KEL events (across all
+    // identifiers) and offers functions for retrieving them, verifying the
+    // integrity of new events, and conducting signature verification.
     let controller = Arc::new(Controller::new(ControllerConfig {
         db_path: database_path.path().to_owned(),
         ..Default::default()
     })?);
 
+    // Incept identifier.
+    // The `IdentifierController` structure facilitates the management of the
+    // Key Event Log specific to a particular identifier.
     let actor = {
         let pk = BasicPrefix::Ed25519(key_manager.public_key());
         let npk = BasicPrefix::Ed25519(key_manager.next_public_key());
@@ -58,6 +64,8 @@ async fn test_kel_with_witness() -> Result<(), ControllerError> {
         IdentifierController::new(identifier, controller.clone(), None)
     };
 
+    // The Event Seal specifies the stage of KEL at the time of signature creation.
+    // This enables us to retrieve the correct public keys from KEL during verification.
     // Trying to get current actor event seal.
     let inception_event_seal = actor.get_last_establishment_event_seal();
 
@@ -84,6 +92,20 @@ async fn test_kel_with_witness() -> Result<(), ControllerError> {
     let inception_event_seal = actor.get_last_establishment_event_seal();
     assert!(inception_event_seal.is_ok());
 
+    // Sign message with established identifier
+    let first_message = "Hi".as_bytes();
+    let first_message_signature = vec![SelfSigningPrefix::Ed25519Sha512(
+        key_manager.sign(first_message).unwrap(),
+    )];
+
+    let signature = actor.transferable_signature(
+        first_message,
+        inception_event_seal?,
+        &first_message_signature,
+    )?;
+
+    assert!(controller.verify(first_message, &signature).is_ok());
+
     // Rotate keys
     key_manager.rotate()?;
     let pk = BasicPrefix::Ed25519(key_manager.public_key());
@@ -109,7 +131,7 @@ async fn test_kel_with_witness() -> Result<(), ControllerError> {
 
     // Verification fails. Rotation wasn't accepted into KEL because of lack of
     // witness receipts. Current event seal points to inception event so old
-    // public keys was used for verification.
+    // public keys are used for verification.
     assert!(matches!(
         controller.verify(message, &signature).unwrap_err(),
         ControllerError::FaultySignature
