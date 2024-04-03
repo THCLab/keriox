d3364c1d199d96ab53ce39775961f78e25fca9ae
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-04-03 16:59:41 +0200

    refactor: add kel managing submodule

---
+++ components/controller/src/identifier/kel_managing.rs
@@ -0,0 +1,201 @@
+use keri_core::{
+    actor::{event_generator, prelude::SelfAddressingIdentifier},
+    event::{event_data::EventData, KeyEvent},
+    event_message::{
+        cesr_adapter::{parse_event_type, EventType},
+        msg::KeriEvent,
+        signed_event_message::{Message, Notice},
+    },
+    oobi::{LocationScheme, Scheme},
+    prefix::{BasicPrefix, IdentifierPrefix, IndexedSignature, SelfSigningPrefix},
+    query::reply_event::{ReplyEvent, ReplyRoute},
+};
+
+use crate::error::ControllerError;
+use keri_core::prefix::CesrPrimitive;
+
+use super::Identifier;
+
+impl Identifier {
+    /// Generate and return rotation event for Identifier
+    pub async fn rotate(
+        &self,
+        current_keys: Vec<BasicPrefix>,
+        new_next_keys: Vec<BasicPrefix>,
+        new_next_threshold: u64,
+        witness_to_add: Vec<LocationScheme>,
+        witness_to_remove: Vec<BasicPrefix>,
+        witness_threshold: u64,
+    ) -> Result<String, ControllerError> {
+        for wit_oobi in &witness_to_add {
+            self.communication.resolve_loc_schema(wit_oobi).await?;
+        }
+
+        let witnesses_to_add = witness_to_add
+            .iter()
+            .map(|wit| {
+                if let IdentifierPrefix::Basic(bp) = &wit.eid {
+                    Ok(bp.clone())
+                } else {
+                    Err(ControllerError::WrongWitnessPrefixError)
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let state = self.known_events.get_state(&self.id)?;
+
+        event_generator::rotate(
+            state,
+            current_keys,
+            new_next_keys,
+            new_next_threshold,
+            witnesses_to_add,
+            witness_to_remove,
+            witness_threshold,
+        )
+        .map_err(|e| ControllerError::EventGenerationError(e.to_string()))
+    }
+
+    /// Generate and return interaction event for Identifier
+    pub fn anchor(&self, payload: &[SelfAddressingIdentifier]) -> Result<String, ControllerError> {
+        let state = self.known_events.get_state(&self.id)?;
+        event_generator::anchor(state, payload)
+            .map_err(|e| ControllerError::EventGenerationError(e.to_string()))
+    }
+
+    /// Checks signatures and updates database.
+    /// Must call [`IdentifierController::notify_witnesses`] after calling this function if event is a key event.
+    pub async fn finalize_event(
+        &mut self,
+        event: &[u8],
+        sig: SelfSigningPrefix,
+    ) -> Result<(), ControllerError> {
+        let parsed_event =
+            parse_event_type(event).map_err(|_e| ControllerError::EventFormatError)?;
+        match parsed_event {
+            EventType::KeyEvent(ke) => {
+                // Provide kel for new witnesses
+                // TODO  should add to notify_witness instead of sending directly?
+                match &ke.data.event_data {
+                    EventData::Rot(rot) | EventData::Drt(rot) => {
+                        let own_kel = self.known_events.find_kel_with_receipts(&self.id).unwrap();
+                        for witness in &rot.witness_config.graft {
+                            let witness_id = IdentifierPrefix::Basic(witness.clone());
+                            for msg in &own_kel {
+                                self.communication
+                                    .send_message_to(
+                                        &witness_id,
+                                        Scheme::Http,
+                                        Message::Notice(msg.clone()),
+                                    )
+                                    .await?;
+                            }
+                        }
+                    }
+                    _ => (),
+                };
+                self.finalize_key_event(&ke, &sig)?;
+                Ok(())
+            }
+            EventType::Rpy(rpy) => match rpy.get_route() {
+                ReplyRoute::EndRoleAdd(_) => {
+                    Ok(self.finalize_add_role(&self.id, rpy, vec![sig]).await?)
+                }
+                ReplyRoute::EndRoleCut(_) => todo!(),
+                _ => Err(ControllerError::WrongEventTypeError),
+            },
+            EventType::Qry(_) => todo!(),
+            EventType::Receipt(_) => todo!(),
+            EventType::Exn(_) => todo!(),
+        }
+    }
+
+    /// Adds signature to event and processes it.
+    /// Should call `IdentifierController::notify_witnesses` after calling this function.
+    fn finalize_key_event(
+        &mut self,
+        event: &KeriEvent<KeyEvent>,
+        sig: &SelfSigningPrefix,
+    ) -> Result<(), ControllerError> {
+        let own_index = self.get_index(&event.data).unwrap();
+        let signature = IndexedSignature::new_both_same(sig.clone(), own_index as u16);
+
+        let signed_message = event.sign(vec![signature], None, None);
+        self.known_events
+            .save(&Message::Notice(Notice::Event(signed_message.clone())))?;
+
+        // let st = self.state.clone().apply(&ke)?;
+        // self.state = st;
+
+        self.to_notify.push(signed_message);
+
+        Ok(())
+    }
+
+    async fn finalize_add_role(
+        &self,
+        signer_prefix: &IdentifierPrefix,
+        event: ReplyEvent,
+        sig: Vec<SelfSigningPrefix>,
+    ) -> Result<(), ControllerError> {
+        let (dest_identifier, messages_to_send) =
+            self.known_events
+                .finalize_add_role(signer_prefix, event, sig)?;
+        // TODO: send in one request
+        for ev in messages_to_send {
+            self.communication
+                .send_message_to(&dest_identifier, Scheme::Http, ev)
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    /// Helper function for getting the position of identifier's public key in
+    /// group's current keys list.
+    pub(crate) fn get_index(&self, key_event: &KeyEvent) -> Result<usize, ControllerError> {
+        match &key_event.event_data {
+            EventData::Icp(icp) => {
+                // TODO what if group participant is a group and has more than one
+                // public key?
+                let own_pk = self.known_events.current_public_keys(&self.id)?[0].clone();
+                icp.key_config
+                    .public_keys
+                    .iter()
+                    .position(|pk| pk.eq(&own_pk))
+            }
+            EventData::Rot(rot) => {
+                let own_npk = &self.known_events.next_keys_hashes(&self.id)?[0];
+                rot.key_config
+                    .public_keys
+                    .iter()
+                    .position(|pk| own_npk.verify_binding(pk.to_str().as_bytes()))
+            }
+            EventData::Dip(dip) => {
+                // TODO what if group participant is a group and has more than one
+                // public key?
+                let own_pk = self.known_events.current_public_keys(&self.id)?[0].clone();
+                dip.inception_data
+                    .key_config
+                    .public_keys
+                    .iter()
+                    .position(|pk| pk.eq(&own_pk))
+            }
+            EventData::Drt(drt) => {
+                let own_npk = &self.known_events.next_keys_hashes(&self.id)?[0];
+                drt.key_config
+                    .public_keys
+                    .iter()
+                    .position(|pk| own_npk.verify_binding(pk.to_str().as_bytes()))
+            }
+            EventData::Ixn(_ixn) => {
+                let own_pk = self.known_events.current_public_keys(&self.id)?[0].clone();
+                self.known_events
+                    .current_public_keys(&key_event.get_prefix())?
+                    .iter()
+                    .position(|pk| pk.eq(&own_pk))
+            }
+        }
+        .ok_or(ControllerError::NotGroupParticipantError)
+    }
+}
--- components/controller/src/identifier/test.rs
+++ components/controller/src/identifier/test.rs
@@ -1,5 +1,9 @@
 use cesrox::primitives::codes::self_addressing::SelfAddressing;
-use keri_core::{actor::prelude::HashFunction, prefix::{BasicPrefix, SelfSigningPrefix}, signer::{CryptoBox, KeyManager}};
+use keri_core::{
+    actor::prelude::HashFunction,
+    prefix::{BasicPrefix, SelfSigningPrefix},
+    signer::{CryptoBox, KeyManager},
+};
 use tempfile::Builder;
 
 use crate::{config::ControllerConfig, controller::Controller, error::ControllerError};
@@ -8,23 +12,24 @@ use crate::{config::ControllerConfig, controller::Controller, error::ControllerE
 async fn test_kel_managing() -> Result<(), ControllerError> {
     let root = Builder::new().prefix("test-db").tempdir().unwrap();
 
-	let controller = Controller::new(ControllerConfig {
+    let controller = Controller::new(ControllerConfig {
         db_path: root.path().to_owned(),
         ..Default::default()
     })?;
 
     let mut km = CryptoBox::new()?;
-	
-	let first_pk = BasicPrefix::Ed25519(km.public_key());
+
+    let first_pk = BasicPrefix::Ed25519(km.public_key());
     let first_next_npk = BasicPrefix::Ed25519(km.next_public_key());
-	let inception_event = controller.incept(vec![first_pk.clone()], vec![first_next_npk], vec![], 0).await?;
+    let inception_event = controller
+        .incept(vec![first_pk.clone()], vec![first_next_npk], vec![], 0)
+        .await?;
 
-	let signature = SelfSigningPrefix::Ed25519Sha512(km.sign(inception_event.as_bytes())?);
+    let signature = SelfSigningPrefix::Ed25519Sha512(km.sign(inception_event.as_bytes())?);
 
-	let mut identifier = controller
-		.finalize_incept(inception_event.as_bytes(), &signature)?;
+    let mut identifier = controller.finalize_incept(inception_event.as_bytes(), &signature)?;
 
-	let keys = identifier.current_public_keys()?;
+    let keys = identifier.current_public_keys()?;
     assert_eq!(keys, vec![first_pk.clone()]);
 
     // Keys rotation
@@ -32,18 +37,25 @@ async fn test_kel_managing() -> Result<(), ControllerError> {
     let second_pk = BasicPrefix::Ed25519(km.public_key());
     let second_next_pk = BasicPrefix::Ed25519(km.next_public_key());
     let rotation_event = identifier
-        .rotate(vec![second_pk.clone()], vec![second_next_pk], 1, vec![], vec![], 0)
+        .rotate(
+            vec![second_pk.clone()],
+            vec![second_next_pk],
+            1,
+            vec![],
+            vec![],
+            0,
+        )
         .await?;
 
     let signature = SelfSigningPrefix::Ed25519Sha512(km.sign(rotation_event.as_bytes())?);
     identifier
         .finalize_event(rotation_event.as_bytes(), signature)
         .await?;
-    
-	let keys = identifier.current_public_keys()?;
+
+    let keys = identifier.current_public_keys()?;
     assert_ne!(keys, vec![first_pk]);
     assert_eq!(keys, vec![second_pk.clone()]);
-    
+
     let data_to_anchor = b"Hello world";
     let said = HashFunction::from(SelfAddressing::Blake3_256).derive(data_to_anchor);
     let interaction_event = identifier.anchor(&[said])?;
@@ -56,6 +68,6 @@ async fn test_kel_managing() -> Result<(), ControllerError> {
     assert_eq!(keys, vec![second_pk]);
     let state = identifier.known_events.get_state(&identifier.id);
     assert_eq!(state.unwrap().sn, 2);
-    
+
     Ok(())
 }
