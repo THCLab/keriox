1e9d5a032f981d8a0d5b7f15c286de535605e741
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-05-15 17:19:28 +0200

    fix: don't save the same receipt twice

--- keriox_core/src/database/mod.rs
+++ keriox_core/src/database/mod.rs
@@ -141,10 +141,17 @@ impl SledEventDatabase {
         receipt: SignedNontransferableReceipt,
         id: &IdentifierPrefix,
     ) -> Result<(), DbError> {
-        self.receipts_nt
-            .push(self.identifiers.designated_key(id)?, receipt)?;
-        self.db.flush()?;
+        let receipt_body = receipt.body;
+        let sigs = receipt.signatures;
+        for sig in sigs {
+            let single_receipt = SignedNontransferableReceipt { body: receipt_body.clone(), signatures: vec![sig] };
+            if !self.receipts_nt.contains_value(&single_receipt) {
+                self.receipts_nt.push(self.identifiers.designated_key(id)?, single_receipt)?;
+                self.db.flush()?;
+            }
+        };
         Ok(())
+        
     }
 
     pub fn get_receipts_nt(
