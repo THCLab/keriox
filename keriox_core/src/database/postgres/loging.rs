971a04d5ea8064fedd68aca8f1b45e0e8a1068b8
Zdravko Iliev <zdravko.iliev@vereign.com>
2026-01-21 16:20:33 +0200

    fix: cleanup

--- keriox_core/src/database/postgres/loging.rs
+++ keriox_core/src/database/postgres/loging.rs
@@ -1,11 +1,6 @@
 use crate::{
     database::{postgres::error::PostgresError, LogDatabase as LogDatabaseTrait},
-    event::KeyEvent,
-    event_message::{
-        msg::KeriEvent,
-        signature::{Nontransferable, Transferable},
-        signed_event_message::SignedEventMessage,
-    },
+    event_message::signature::{Nontransferable, Transferable},
     prefix::IndexedSignature,
 };
 
@@ -31,99 +26,6 @@ impl PostgresLogDatabase {
 
     pub fn pool(&self) -> &PgPool {
         &self.pool
-    }
-
-    /// Async: log a signed event (stores event + signatures + receipts)
-    pub async fn log_event_async(
-        &self,
-        signed_event: &SignedEventMessage,
-    ) -> Result<(), PostgresError> {
-        // 1. Insert the event
-        self.insert_key_event_async(&signed_event.event_message)
-            .await?;
-
-        // 2. Insert signatures
-        let digest = signed_event
-            .event_message
-            .digest()
-            .map_err(|_e| PostgresError::MissingDigest)?;
-
-        self.insert_indexed_signatures_async(&digest, &signed_event.signatures)
-            .await?;
-
-        // 3. Insert witness receipts if present
-        if let Some(wits) = &signed_event.witness_receipts {
-            self.insert_nontrans_receipt_async(&digest, wits).await?;
-        }
-
-        Ok(())
-    }
-
-    /// Async: insert a key event into the events table
-    async fn insert_key_event_async(
-        &self,
-        event: &KeriEvent<KeyEvent>,
-    ) -> Result<(), PostgresError> {
-        let digest = event.digest().map_err(|_e| PostgresError::MissingDigest)?;
-        let key = rkyv::to_bytes::<rkyv::rancor::Error>(&digest)?;
-        let value = rkyv::to_bytes::<rkyv::rancor::Error>(event)?;
-
-        sqlx::query(
-            "INSERT INTO events (digest, event_data) VALUES ($1, $2)
-             ON CONFLICT (digest) DO NOTHING",
-        )
-        .bind(key.as_slice())
-        .bind(value.as_slice())
-        .execute(&self.pool)
-        .await?;
-
-        Ok(())
-    }
-
-    /// Async: insert indexed signatures
-    async fn insert_indexed_signatures_async(
-        &self,
-        said: &said::SelfAddressingIdentifier,
-        signatures: &[IndexedSignature],
-    ) -> Result<(), PostgresError> {
-        let serialized_said = rkyv::to_bytes::<rkyv::rancor::Error>(said)?;
-
-        for sig in signatures {
-            let sig_bytes = rkyv::to_bytes::<rkyv::rancor::Error>(sig)?;
-            sqlx::query(
-                "INSERT INTO signatures (digest, signature_data) VALUES ($1, $2)
-                 ON CONFLICT (digest, signature_data) DO NOTHING",
-            )
-            .bind(serialized_said.as_slice())
-            .bind(sig_bytes.as_slice())
-            .execute(&self.pool)
-            .await?;
-        }
-
-        Ok(())
-    }
-
-    /// Async: insert non-transferable receipts
-    async fn insert_nontrans_receipt_async(
-        &self,
-        said: &said::SelfAddressingIdentifier,
-        nontrans: &[Nontransferable],
-    ) -> Result<(), PostgresError> {
-        let serialized_said = rkyv::to_bytes::<rkyv::rancor::Error>(said)?;
-
-        for receipt in nontrans {
-            let receipt_bytes = rkyv::to_bytes::<rkyv::rancor::Error>(receipt)?;
-            sqlx::query(
-                "INSERT INTO nontrans_receipts (digest, receipt_data) VALUES ($1, $2)
-                 ON CONFLICT (digest, receipt_data) DO NOTHING",
-            )
-            .bind(serialized_said.as_slice())
-            .bind(receipt_bytes.as_slice())
-            .execute(&self.pool)
-            .await?;
-        }
-
-        Ok(())
     }
 }
 //TODO: provide sync wrapper around the async methods using block_in_place ??
